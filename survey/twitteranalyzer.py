327711b6f2a342be7e1e4ebb69840ffa8e11a471
David Lance <dcl71@case.edu>
2016-07-25 10:36:52 -0400

    Update .gitignore

--- survey/twitteranalyzer.py
+++ survey/twitteranalyzer.py
@@ -8,6 +8,7 @@ import json
 import twitter
 import config
 
+
 def convert_status_to_pi_content_item(s):
     # My code here
     return {
@@ -26,25 +27,25 @@ def convert_status_to_pi_content_item(s):
 handle = sys.argv[1]
 
 twitter_api = twitter.Api(consumer_key=config.twitter_consumer_key,
-                  consumer_secret=config.twitter_consumer_secret,
-                  access_token_key=config.twitter_access_token,
-                  access_token_secret=config.twitter_access_secret,)
+                          consumer_secret=config.twitter_consumer_secret,
+                          access_token_key=config.twitter_access_token,
+                          access_token_secret=config.twitter_access_secret, )
 
 statuses = twitter_api.GetUserTimeline(screen_name=handle,
-                  count=400,
-                  include_rts=False)
+                                       count=400,
+                                       include_rts=False)
 
 pi_content_items_array = map(convert_status_to_pi_content_item, statuses)
-pi_content_items = { 'contentItems' : pi_content_items_array }
+pi_content_items = {'contentItems': pi_content_items_array}
 
 r = requests.post(config.pi_url + '/v2/profile',
-    auth=(config.pi_username, config.pi_password),
-    headers = {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-    },
-    data=json.dumps(pi_content_items)
-)
+                  auth=(config.pi_username, config.pi_password),
+                  headers={
+                      'content-type': 'application/json',
+                      'accept': 'application/json'
+                  },
+                  data=json.dumps(pi_content_items)
+                  )
 
 print("Profile Request sent. Status code: %d, content-type: %s" % (r.status_code, r.headers['content-type']))
 print json.loads(r.text)
